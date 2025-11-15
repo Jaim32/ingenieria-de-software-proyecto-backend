f71fc37ce995719de733163a4d433efe60be0e33
Jaim32 <00071322@uca.edu.sv>
2025-11-14 23:18:02 -0600

    add PostResponseDto

---
+++ LifeSync-Backend/src/main/java/org/example/lifesyncbackend/Domain/DTO/response/PostResponseDTO.java
@@ -0,0 +1,35 @@
+package org.example.lifesyncbackend.Domain.DTO.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class PostResponseDTO {
+
+    @JsonProperty("idPost")
+    private Long idPost;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("content")
+    private String content;
+
+    @JsonProperty("image")
+    private String image;
+
+    @JsonProperty("createdAt")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("updatedAt")
+    private LocalDateTime updatedAt;
+
+    @JsonProperty("userId")
+    private Long userId;
+}
