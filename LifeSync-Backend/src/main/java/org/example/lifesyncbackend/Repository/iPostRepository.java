d0aacfe6127a2f191bef9a9592f94892ab0e9be6
Jaim32 <00071322@uca.edu.sv>
2025-11-14 23:26:04 -0600

    add repositori base

---
+++ LifeSync-Backend/src/main/java/org/example/lifesyncbackend/Repository/iPostRepository.java
@@ -0,0 +1,8 @@
+package org.example.lifesyncbackend.Repository;
+
+import org.example.lifesyncbackend.Domain.Entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface iPostRepository extends JpaRepository<Post, Long> {
+    // Métodos adicionales si es necesario
+}
